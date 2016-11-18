9ee2f7e6269544eceacfca8b77feef6aec56a18e
yuga yamamoto <yamamotojyuku0611@gmail.com>
2016-11-18 14:18:10 +0900

    add new file

---
+++ kadai.rb
@@ -0,0 +1,20 @@
+
+a = Random.rand(1..100)
+
+puts "please enter the number"
+
+loop{
+score = gets.to_i
+
+
+	if a < score 
+		puts "more small!"
+	elsif a > score 
+		puts "more large!"
+	else 
+		puts "that's right!"
+		break
+	end
+puts "please enter another number"
+
+}
